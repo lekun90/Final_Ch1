9f5d50e48a51544f515024dd49e9d6782235a0ae
lekun90 <lekun90@mail.ru>
2022-10-06 21:41:46 +0300

    Add file realisation cod

---
+++ Itog_CH1/Program.cs
@@ -0,0 +1,36 @@
+﻿string[] GetShortWordsArray (string[] array, int MaxLength)
+{
+    // новый массив строк, длина которые не превышает MaxLength символов
+    string[] result = new string[array.Length];
+    int count = 0;
+    for (int i=0; i<array.Length; i++)
+    {
+        if (array[i].Length <= MaxLength)
+        {
+            result[count]=array[i];
+            count++;
+        }
+    }
+    // изменяем количество элементов массива до указанной величины
+    Array.Resize(ref result,count);
+    return result;
+}
+
+Console.Write("Количество символов нового массива не должно превышать: ");
+int MaxStrLen = Convert.ToInt32(Console.ReadLine());
+ 
+Console.Write("Введите массив строк через пробел: ");
+string[] array = Console.ReadLine().Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+string[] shortArray = GetShortWordsArray (array,MaxStrLen);
+Console.WriteLine("Результирующий массив:");
+if (shortArray.Length == 0)
+{
+    Console.WriteLine("Массив пуст");
+}
+else
+{
+    Array.ForEach(shortArray, (str)=>Console.Write($"{str}"));
+    Console.WriteLine();
+}
+
